a2e748824f67968bd58fc564e2c7af67bda69e7f
furkancanbicim <furkancanbicim@gmail.com>
2022-06-05 16:53:27 +0300

    firt commit

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isim ve soyisimli müşteri, {2} telefon numarasıyla birlikte eklendi ve id'si {3} verildi", musteri.Name, musteri.Surname,musteri.Phone,musteri.Id);
+        }
+        public void MusteriListele(List<Musteri> musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Adı : " + musteri.Name);
+                Console.WriteLine("Soyadı : " + musteri.Surname);
+                Console.WriteLine("Phone : " + musteri.Phone);
+            }
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isim ve soyisimli müşteri, {2} telefon numarasıyla birlikte silindi", musteri.Name, musteri.Surname, musteri.Phone);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassMetotDemo;
+
+
+Musteri musteri = new Musteri() 
+{
+Id = 1,
+Name = "Ali",
+Surname = "Veli",
+Phone = "0555555555" 
+};
+
+Musteri musteri1 = new Musteri()
+{
+    Id = 2,
+    Name = "Mehmet",
+    Surname = "Sura",
+    Phone = "05444444444"
+};
+
+List<Musteri>musteriler = new List<Musteri>();
+musteriler.Add(musteri);
+musteriler.Add(musteri1);
+
+MusteriManager MusteriM = new MusteriManager();
+MusteriM.MusteriEkle(musteri);
+MusteriM.MusteriEkle(musteri1);
+
+Console.WriteLine("\nMüşteri Listesi :");
+MusteriM.MusteriListele(musteriler);
+
+Console.WriteLine("\n--------------------------------------------------------------");
+
+MusteriM.MusteriSil(musteri);
+MusteriM.MusteriSil(musteri1);
